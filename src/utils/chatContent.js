a907373d0afe54f537e749d86299bf3d84933d94
JeremyLavigne <lavigne.jeremy@post.com>
2020-09-06 13:38:05 +0200

    remove images from .gitignore

---
+++ src/utils/chatContent.js
@@ -0,0 +1,67 @@
+const chatContent = [
+    {
+        group: 0,
+        buttons: [
+            { index: 0, text: "Start Casually", disabled: null },
+            { index: 1, text: "Start Seriously", disabled: null }
+        ]
+    },
+    {
+        group: 1,
+        buttons: [
+            { index: 0, text: "General Humor?", disabled: null },
+            { index: 1, text: "Life in Stockholm?", disabled: null },
+            { index: 2, text: "Family?", disabled: null },
+            { index: 3, text: "Next", disabled: null }
+        ],
+        answers: [
+            "Good!",
+            "Wonderful, we live in Stora Essingen. It is nice to live close to everything we need.",
+            "My wife works as a customer representative in Södermälm, and my sons (8 and 5) go to school."
+        ]
+    },
+    {
+        group: 2,
+        buttons: [
+            { index: 0, text: "Move in Sweden?", disabled: null },
+            { index: 1, text: "Changing job?", disabled: null },
+            { index: 2, text: "Plan for future?", disabled: null },
+            { index: 3, text: "Next", disabled: null }
+        ],
+        answers: [
+            "Part of a big family project. Sweden seems to be the best country to raise children.",
+            "Used to had this feeling to be locked in the same job for all life. IT world offers many ways and possibilities.",
+            "I want to learn swedish, buy a house, a car, ... Just enjoy the life!"
+        ]
+    },
+    {
+        group: 3,
+        buttons: [
+            { index: 0, text: "Best Qualities?", disabled: null },
+            { index: 1, text: "Work?", disabled: null },
+            { index: 2, text: "Work in team?", disabled: null },
+            { index: 3, text: "Next", disabled: null }
+        ],
+        answers: [
+            "Always positive and able to adapt.",
+            "Work is not only skills (coding here), but also deadlines, customer requests, a global project, ... This kind of experience can be found in every profession.",
+            "Sharing and mutual aid, working for a common goal. And sometimes admit that team can come before my ideas (which are always the best :) )."
+        ]
+    },
+    {
+        group: 4,
+        buttons: [
+            { index: 0, text: "Web Dev?", disabled: null },
+            { index: 1, text: "Software Dev?", disabled: null },
+            { index: 2, text: "Other?", disabled: null },
+            { index: 3, text: "Next", disabled: null }
+        ],
+        answers: [
+            "The web was my point of entry. I love the 'immediate' result of actions: thinking Front-end with React for example.",
+            "I really discovered it in SDA with Novare. I love the deep thinking it takes.",
+            "Curiosity and open-mindedness always lead to something great."
+        ]
+    }
+]
+
+export default chatContent
